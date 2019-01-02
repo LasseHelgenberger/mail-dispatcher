edf080b426234c785aefa9b85ff5e9c733a6774c
Lasse Helgenberger <github.com@lasse.cc>
2019-01-02 10:51:08 +0100

    First structure to mail-dispatcher.php

--- credentials.php
+++ credentials.php
@@ -12,6 +12,7 @@ This file is part of the "mail-dispatcher"-Script by Lasse Helgenberger.
 For further information go to
 https://github.com/LasseHelgenberger/mail-dispatcher
 or contact mail-dispatcher@lasse.cc
+
 */
 
 
---
+++ mail-dispatcher.php
@@ -0,0 +1,24 @@
+<?php
+/*
+
+##################################
+##	MAIL-DISPATCHER		##
+##################################
+
+This file is part of the "mail-dispatcher"-Script by Lasse Helgenberger.
+For further information go to
+https://github.com/LasseHelgenberger/mail-dispatcher
+or contact mail-dispatcher@lasse.cc
+
+*/
+
+maildispatcher();
+
+function maildispatcher() {
+
+// Check for new Mail
+
+// If new Mail: Send to everyone
+}
+
+?>
