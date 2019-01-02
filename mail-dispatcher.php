4ef336f87ccc60c64c708fe359d8b890617d6d01
Lasse Helgenberger <github.com@lasse.cc>
2019-01-02 12:24:17 +0100

    added sending mails

--- mail-dispatcher.php
+++ mail-dispatcher.php
@@ -17,20 +17,46 @@ maildispatcher();
 function maildispatcher() {
 include 'credentials.php';
 
-// Check for new Mail
+//open imap stream
 $mbox = imap_open($cred_mailbox, $cred_mailuser, $cred_mailpasswd);
 
+//for each message in the inbox...
 for($i = 0; $i < imap_num_msg($mbox); $i++) {
+  // ...get the header info
   $header = imap_headerinfo($mbox, $i+1);
   $from = $header->fromaddress;
   $subject = $header->Subject;
 echo "FROM: ".$from."\n";
 echo "SUBJECT: ".$subject."\n";
-  echo "BODY:".imap_body($mbox, $i+1)."\n\n";
-}
+  // ... get the body
+  $body = imap_body($mbox, $i+1);
+echo "BODY:".$body."\n\n";
 
+//TODO check for Attachements
+
+  // ... forward email
+  forwardemail($from, $subject, $body);
+}
 imap_close($mbox);
-// If new Mail: Send to everyone
+
 }
 
+
+function forwardemail($from, $subject, $body) {
+//TODO
+$recipients = getrecipients();
+
+//$mbox = imap_open($cred_mailbox, $cred_mailuser, $cred_mailpasswd);
+
+for($i = 0; $i < sizeof($recipients); $i++) {
+  imap_mail($recipients[$i], $subject, $body);
+}
+}
+
+function getrecipients() {
+//TODO
+
+$res = array("test1@lasse.cc", "test2@lasse.cc", "test3@lasse.cc");
+return $res;
+}
 ?>
