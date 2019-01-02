6c47cde3f031faeec6e8c868d7f7150a8c7f1d16
Lasse Helgenberger <github.com@lasse.cc>
2019-01-02 10:39:46 +0100

    adding mail-dispatcher.php credentials.php

---
+++ credentials.php
@@ -0,0 +1,29 @@
+<?php 
+/*
+
+##################################
+##	CREDENTIALS		##
+##################################
+
+IMPORTANT: Do not commit any passwords to Git!
+
+
+This file is part of the "mail-dispatcher"-Script by Lasse Helgenberger.
+For further information go to
+https://github.com/LasseHelgenberger/mail-dispatcher
+or contact mail-dispatcher@lasse.cc
+*/
+
+
+
+// IMAP CREDENTIALS
+$cred_mailbox = "{localhost:993/imap/ssl}INBOX";
+$cred_mailuser = "mail@address.com";
+$cred_mailpasswd = "passwd";
+
+// MYSQL CREDENTIALS
+$cred_mysqldb = "database";
+$cred_mysqluser = "user";
+$cred_mysqlpasswd = "passwd";
+
+?>
